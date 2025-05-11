a76e3f846da36c6625432aca117e6555b7224b6e
igaozp <igaozp@hotmail.com>
2025-05-11 22:03:20 +0800

    feat: add compiler and scanner implementation

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "vm.h"
 
 #include <stdio.h>
@@ -80,8 +81,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
